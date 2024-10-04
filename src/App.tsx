9b0359135a828b1ea37365b2819eaaaa1c774f5d
Arman Takmazyan <armanworkmail01@gmail.com>
2024-10-04 23:41:16 +0400

    Add an error boundary to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { ApplicationErrorBoundary } from './components/ApplicationErrorBoundary';
 import { Header } from './components/Header';
 import { Layout } from './components/Masonry/styles';
 import './App.css';
 
 const App: React.FC<{}> = () => {
   return (
-    <Layout>
-      <Header />
-      <Outlet />
-    </Layout>
+    <ApplicationErrorBoundary>
+      <Layout>
+        <Header />
+        <Outlet />
+      </Layout>
+    </ApplicationErrorBoundary>
   );
 };
 
