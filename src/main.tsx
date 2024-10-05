b4e20d7286b08fe80a049eed19fccd2bcc2bbd3f
Arman Takmazyan <armanworkmail01@gmail.com>
2024-10-05 13:17:41 +0400

    Add: NotFountPage component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { NotFoundPage } from './components/NotFoundPage';
 import { ImageDetailsPage } from './pages/ImageDetailsPage/index.tsx';
 import { RootPage } from './pages/Root';
 import App from './App.tsx';
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: '/image/:id',
         element: <ImageDetailsPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
   },
